a68364a12b5cc0d762c2b3d3f9fb784bb3403a04
franzo08112007@gmail.com <franzo08112007@gmail.com>
2023-03-24 13:29:18 +0800

    finally done yay

--- Li_Sitchon_3qGFA3/script.js
+++ Li_Sitchon_3qGFA3/script.js
@@ -5,6 +5,12 @@ function randomize() {
     num1 = Math.round(Math.random() * 20);
     num2 = Math.round(Math.random() * 20);
     num3 = Math.round(Math.random() * 20);
+    document.getElementById("num1").innerHTML = num1;
+    document.getElementById("num2").innerHTML = num2;
+    document.getElementById("num3").innerHTML = num3;
     document.getElementById("ticketdiv").style.display = "block"
     document.getElementById("ticketnum").innerHTML = alphabet.charAt(num1 + 1) + Math.max(num1, num2, num3);
+    waithrs = Math.floor((num2 * num3) / 60);
+    waitmins = (num2 * num3) % 60;
+    document.getElementById("waittime").innerHTML = "Your ticket will be resolved in " + waithrs + " hr " + waitmins + " min."
 }
