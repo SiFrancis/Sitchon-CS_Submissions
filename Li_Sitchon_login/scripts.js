b2d37b5a484cc23e54211475335b94ef7e4979c7
franzo08112007@gmail.com <franzo08112007@gmail.com>
2023-03-31 22:28:32 +0800

    TODO: plagiarize 2q footer

--- Li_Sitchon_login/scripts.js
+++ Li_Sitchon_login/scripts.js
@@ -1,3 +1,22 @@
 function submitLogin() {
+    var formname = document.forms["loginform"]["username"].value;
+    var formpass = document.forms["loginform"]["password"].value;
+    var needed = "";
+    if ((formname == null || formname == "") || (formpass == null || formpass == "")) {
+        document.getElementById("logincheck").style.display = "block";
+    }
+    if (formpass == null || formpass == "") {
+        needed = "password";
+        if (formname == null || formname == "") {
+            needed = "username and password";
+        }
+    }
+    if (formname == null || formname == "") {
+        needed = "username";
+        if (formpass == null || formpass == "") {
+            needed = "username and password";
+        }
+    }
     
+    document.getElementById("logincheck").innerHTML = ("Please enter your " + needed + ".");
 }
